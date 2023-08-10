4237562228113347de02b67e5c2304785dbbcb20
octoandzl <octoandzl@gmail.com>
2023-08-10 15:12:16 +0200

    have added cmd.py file

---
+++ cmd.py
@@ -0,0 +1,9 @@
+# git pull --recurse-submodules
+# git config submodule.recurse true
+
+# git clone
+# git submodule update --init
+
+# synchro d'un tag
+# dans le submodule -git checkout tag -git push
+# dans le module -git submodule update --remote
